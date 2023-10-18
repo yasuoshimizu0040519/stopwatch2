c8f62bf15727437f1f4e80abcb4d482ec4dcbb8e
yasuoshimizu0040519 <145641043+yasuoshimizu0040519@users.noreply.github.com>
2023-10-18 20:40:45 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click", () => {
+    if(int !== null) {
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer, 10);
+});
+
+document.getElementById("stop-timer").addEventListener("click", () => {
+    clearInterval(int);
+});
+
+document.getElementById("reset-timer").addEventListener("click", () => {
+    clearInterval(int);
+    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+    timeRef.innerHTML = "00 : 00 : 00 : 00 ";
+}); 
+
+function displayTimer() {
+    //milliseconds += 10;
+    milliseconds += 1;
+    // if(milliseconds == 1000) {
+    if(milliseconds == 100) {
+        milliseconds = 0;
+        seconds++;
+        if(seconds == 60) {
+            seconds = 0;
+            minutes++;
+            if(minutes == 60) {
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = 
+        milliseconds < 10
+        // ? "00" + milliseconds
+        ? "00" + milliseconds
+        : milliseconds < 100
+        // ? "0" + milliseconds
+        ? milliseconds
+        : milliseconds;
+
+    timeRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+
+}
